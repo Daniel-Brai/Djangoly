d5617d1cd5ed88383c877923ee38cf5676cdda3a
Daniel Brai <braidaniel2001@gmail.com>
2022-12-22 05:47:10 +0100

    Feat: Implemented FBVs

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.urls import reverse
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -16,7 +16,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                                 on_delete=models.CASCADE,
                                 related_name='blog_posts')
@@ -38,5 +38,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    # def get_absolute_url(self):
-    #     return reverse("post_detail", kwargs={"pk": self.pk})
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-# from .views import BlogDetailView, BlogListView
 from .views import post_detail, post_list
 
 app_name = 'blog'
 urlpatterns = [
     path('', post_list, name='post_list'),
-    path('<int:id>/', post_detail, name='post_detail'),
-    # path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"), 
-    # path("", BlogListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
 ]
